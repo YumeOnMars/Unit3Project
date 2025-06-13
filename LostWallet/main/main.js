8ec4480c286b9f105913dba01f055581b788c21c
YumeOnMars <bryanwatermelon1@gmail.com>
2025-06-13 13:25:40 -0400

    boom

--- LostWallet/main/main.js
+++ LostWallet/main/main.js
@@ -8,11 +8,53 @@ let OpenButton = document.querySelector("#Open")
 
 let WalkButton = document.querySelector("#Walk");
 
+let takeButton = document.querySelector("#TakeMoney")
+
+let ReturnWallet = document.querySelector("#ReturnWallet");
+
+let ReturnItPage = document.querySelector(".ReturnItPage")
+
+let StoleItPage = document.querySelector(".StoleIt");
+
+let AtHisHouse = document.querySelector(".AtHisHouse");
+
+let yesButton = document.querySelector('GoYes');
+
+let BeatUpPage = document.querySelector(".BeatUp");
+
+let BeatUpButton = document.querySelector("#BeatUpRestart");
+
+let fight = document.querySelector('#FightHim')
 OpenButton.addEventListener("click", function() {
     background.style.backgroundImage = "url('openwallet.png')";
     beginning.style.display = "none";
     OpenedPage.style.display = "inline";
 });
+
+takeButton.addEventListener("click", function() {
+    background.style.backgroundImage = "url('StoleWallet.png')";
+    OpenedPage.style.display = "none";
+    StoleItPage.style.display = "inline";
+});
+
+ReturnWallet.addEventListener("click", function() {
+    background.style.backgroundImage = "url('map.jpg')";
+    OpenedPage.style.display = "none";
+    ReturnItPage.style.display = "inline";
+})
+
+GoYes.addEventListener("click", function() {
+    background.style.backgroundImage = "url('happyman.png')";
+    ReturnItPage.style.display = "none";
+    AtHisHouse.style.display = "inline";
+})
+
+fight.addEventListener("click", function() {
+        background.style.backgroundImage = "url('BeatUp.png')";
+    AtHisHouse.style.display = "none";
+    BeatUpPage.style.display = "inline";
+})
+
 
 
 
